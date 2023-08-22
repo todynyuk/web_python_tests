1ac5d0e4258e9ad514f5524df25c04e98c78a82d
todynyuk <todynyuk@solvd.com>
2023-08-22 16:57:08 +0300

    change gitignore and conftest.py

--- conftest.py
+++ conftest.py
@@ -1,33 +1,10 @@
 import pytest
-import os
-from datetime import datetime
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import ChromiumOptions
 
 
 @pytest.fixture(autouse=True, scope='function')
 def driver():
-    # driver = webdriver.Chrome(ChromeDriverManager().install())
-    # driver.maximize_window()
-    # yield driver
-    # driver.quit()
-#-------------------------------------test-------------------------------------
-    # chrome_options = ChromiumOptions()
-    #
-    # service = Service(ChromeDriverManager().install())
-    #
-    # driver = webdriver.Chrome(chrome_options=chrome_options, service=service)
-    # driver.get("http://www.python.org")
     driver = webdriver.Chrome()
-    # s = Service('driver/chromedriver')
-    # driver = webdriver.Chrome(service=s)
     driver.maximize_window()
     yield driver
     driver.quit()
-#--------------------------------------end test----------------------------------
-
-# def pytest_configure(conf):
-#     os.environ["REPORTING_RUN_DISPLAY_NAME"] = f"Rozetka_pytest - {datetime.now().date()}"
